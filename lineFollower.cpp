71f98b1d934665c5d37758b0374df8ae5750b1ab
Jacob <wolstenholme.jacob@gmail.com>
2017-05-09 14:50:14 +1200

    Typo and change array length calculation

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -11,10 +11,10 @@ int main() {
 		int pError = 0;
 		int whiteCount = 0; // Not used yet, but should count properly
 		take_picture();
-		for (int i = 0; i < (sizeof(midArray)/sizeof(midArray[0]); i++) { // sizeof(midArray) returns the size of the whole array in bytes, divided by the size of one element in bytes = number of elements
+		for (int i = 0; i < 64; i++) { // sizeof(midArray) returns the size of the whole array in bytes, divided by the size of one element in bytes = number of elements
 			if (get_pixel(120, i*5, 3) >= 128) {
 				midArray[i] = 1;
-				pError += midArrary[i]*(i-31); // The centre of the array is index 31 & 32. This maps index 31 as 0
+				pError += midArray[i]*(i-31); // The centre of the array is index 31 & 32. This maps index 31 as 0
 				whiteCount++;
 			} else { //get_pixel(120, 1*5, 3) <= 127
 				midArray[i] = 0;
