e5b92b1a22f873c346e285f0a35cbaa52baa6807
Jacob <wolstenholme.jacob@gmail.com>
2017-05-16 14:00:47 +1200

    Remove un-needed array

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -7,17 +7,13 @@ int main() {
 	int cruiseSpeed = 20;
 	double errScale = 175/646; // Not calibrated yet. Requires trial and error to find.
 	while (true) {
-		int midArray [64] = { };
 		double pError = 0;
 		int whiteCount = 0; // Not used yet, but should count properly
 		take_picture();
 		for (int i = 0; i < 64; i++) { // sizeof(midArray) returns the size of the whole array in bytes, divided by the size of one element in bytes = number of elements
 			if (get_pixel(120, i*5, 3) >= 128) {
-				midArray[i] = 1;
-				pError += midArray[i]*(i-31); // The centre of the array is index 31 & 32. This maps index 31 as 0
+				pError += i-31; // The centre of the array is index 31 & 32. This maps index 31 as 0
 				whiteCount++;
-			} else { //get_pixel(120, 1*5, 3) <= 127
-				midArray[i] = 0;
 			}
 		}
 		printf("Counted %d white pixels\n", whiteCount);
