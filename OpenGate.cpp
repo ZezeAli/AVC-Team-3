831b053ee45d2dffc6c1bc3b5cab097b05d93b83
Gabriela <gaby@perez.co.nz>
2017-05-19 18:53:09 +1200

    Update OpenGate.cpp

--- OpenGate.cpp
+++ OpenGate.cpp
@@ -1,32 +1,34 @@
-
 // code to open gate 
 #include <stdio.h>
 #include <time.h>
 #include "E101.h"
 #include <math.h>
 
+//init();
 int main(){
 
 bool gate=true;
+//bool startQuad2=false;
+
 // initialise variables 
 
 while(gate){
-  char addr[15]=('1','3','0','.','1','9','5','.','6','.','1','9','6')
+  char addr[15]={'1', '3','0','.','1','9','5','.','6','.','1','9','6'};
   connect_to_server(addr, 1024);
-  char msgToSrvr[24]=('P','l','e','a','s','e')
+  char msgToSrvr[24]={'P','l','e','a','s','e'};
   send_to_server(msgToSrvr);
   char msgFromSrvr[24];
-  recieve_from_server (msgFromSrvr); 
+  receive_from_server (msgFromSrvr); 
    for(int i = 0; i < 6 ; i++ ){ // copy 6 elements of mesgFromSrvr to msgtosrvr
-     char msgToSrvr [i] = char msgFromSrvr [i];
+      msgToSrvr [i] = msgFromSrvr [i];
    }
   
   send_to_server(msgToSrvr);
 
 
 
-gate=false //end while loop
-startQuad2=true // start
+gate=false; //end while loop
+
+//startQuad2=true; // start
   
 }
-
