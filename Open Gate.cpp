e61e95f40cd4e7206da3981582415c4b6d959072
Gabriela <gaby@perez.co.nz>
2017-05-19 18:11:42 +1200

    Open Gate

---
+++ Open Gate.cpp
@@ -0,0 +1,25 @@
+// code to open gate 
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+#include <math.h>
+
+int main(){
+
+bool gate=true;// initialise variables 
+
+while(gate){
+  char addr[15]=('1','3','0','.','1','9','5','.','6','.','1','9','6')
+  connect_to_server(addr, 1024);
+  char.msgToSrvr[24]=('P','l','e','a','s','e')
+  send_to_server(msgToSrvr);
+  char msgFromSrvr[24];
+  recieve_from_server (msgFromSrvr); // copy 6 elements of mesgFromSrvr to msgtosrvr;
+  send.to.server(msgToSrvr);
+
+
+
+gate=false //end while loop
+
+}
+
