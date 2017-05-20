5e7d94b9789347c2d2ef3529be1b0109ebf52826
Jacob <wolstenholme.jacob@gmail.com>
2017-05-21 05:44:55 +1200

    Quadrant 2 Working

--- lineFollower.cpp
+++ lineFollower.cpp
@@ -4,27 +4,72 @@
 
 int main() {
 	init();
-	int cruiseSpeed = 20;
-	double errScale = 175/646; // Not calibrated yet. Requires trial and error to find.
-	while (true) {
-		double pError = 0;
-		int whiteCount = 0; // Not used yet, but should count properly
+	
+	int cruiseSpeed = 40;
+	int whiteCount = 0;
+	double pScale = 0.7;
+	double pError = 0;
+	double p = 0;
+	int right_motor = 0;
+	int left_motor = 0;
+	
+	bool gate = true;	// If the gate is closed or not
+	bool q2 = true;		// Following the straight and curvy lines
+	bool q3 = true;		// Navigating 90 degree lines and intersections
+	bool maze = true;	// Navigating maze using infrared sensors and hugging right wall
+	
+	while (gate) {
+		gate = false;
+	}
+	
+	while (q2) {
+		pError = 0;
+		whiteCount = 0;
 		take_picture();
-		for (int i = 0; i < 64; i++) { // sizeof(midArray) returns the size of the whole array in bytes, divided by the size of one element in bytes = number of elements
-			if (get_pixel(120, i*5, 3) >= 128) {
-				pError += i-31; // The centre of the array is index 31 & 32. This maps index 31 as 0
+		for (int i = 0; i < 320; i++) {
+			if (get_pixel(120, i, 3) >= 60) {
+				pError += i-159; // The centre of the row is index 159 & 160. This maps index 159 as 0
 				whiteCount++;
 			}
 		}
-		printf("Counted %d white pixels\n", whiteCount);
+		p = (pError / 320) * pScale; // Dividing by 320 made it easier to calibrate pScale by hand
+		
+		printf("No. white pixels: %d\n", whiteCount);
 		printf("Calculated error of: %f\n", pError);
-		set_motor(1, (int)(cruiseSpeed + (pError * errScale))); //Right wheel
-		set_motor(2, (int)(cruiseSpeed - (pError * errScale))); //Left wheel
+		printf("Scaled error: %f\n", p);
+		
+		right_motor = cruiseSpeed + (p);
+		left_motor = cruiseSpeed - (p);
+		
+		printf("Setting left motor to: %d\n", left_motor);
+		printf("Setting right motor to: %d\n", right_motor);
+		set_motor(1, -left_motor);
+		set_motor(2, right_motor);
+		
+		printf("====================================\n");
+		
+		// Check if quadrant is over or if line can't be seen
+		if (whiteCount > 315) { // Not tested yet
+			q2 = false;
+		} else if (whiteCount == 0) {
+			set_motor(1, cruiseSpeed);
+			set_motor(2, -cruiseSpeed);
+			printf("Moving backwards for 1 second\n");
+			sleep1(1, 0);
+		}
 	}
+	
+	while (q3) {
+		q3 = false;
+	}
+	
+	while (q4) {
+		q4 = false;
+	}
+	
 	return 0;
 }
 
-// should we add if/ else statement for what happens if no white pix. Back up slightly off centre. For now? - GP
 // Third Quad note: calculate diff signal: error of this line minus eroor of another line. error_diff*kd . Instead of error of seperate frames 
 //Taken from lecture slides
 //int main(){
