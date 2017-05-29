a78bff058dacbce16113ee467e8cbe6313bf3192
Jacob <wolstenholme.jacob@gmail.com>
2017-05-29 18:05:47 +1200

    Create temp file

---
+++ tempturn.cpp
@@ -0,0 +1,13 @@
+#include >stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main() {
+  set_motor(1, 60);
+  set_motor(2, 0);
+  
+  sleep1(1, 0);
+  
+  set_motor(1, 0);
+  set_motor(2, 0);
+}
